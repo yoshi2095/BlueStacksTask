189d2bb8e682be8700eec7bab3f744925e3c71ad
yoshi2095 <arora4.yoshi@gmail.com>
2020-09-06 15:57:12 +0530

    Updates

---
+++ precache-manifest.1e7202b078733e88e89467b9aeb5b591.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d24f9e5455c0aae5910267d9b17c0e41",
+    "url": "/BlueStacksTask/index.html"
+  },
+  {
+    "revision": "dccca8a6732916d0b62b",
+    "url": "/BlueStacksTask/static/css/main.30021a4a.chunk.css"
+  },
+  {
+    "revision": "6d45a37e022b51b39124",
+    "url": "/BlueStacksTask/static/js/2.98f7a148.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/BlueStacksTask/static/js/2.98f7a148.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dccca8a6732916d0b62b",
+    "url": "/BlueStacksTask/static/js/main.df88c702.chunk.js"
+  },
+  {
+    "revision": "f0cf2ca2b58490529883",
+    "url": "/BlueStacksTask/static/js/runtime-main.8dec9750.js"
+  },
+  {
+    "revision": "7173cfe7840a5f2d3853b4220f6cf851",
+    "url": "/BlueStacksTask/static/media/Bitmap.7173cfe7.png"
+  },
+  {
+    "revision": "90640a8778af0217304f5effb1aa8413",
+    "url": "/BlueStacksTask/static/media/Bitmap.90640a87.png"
+  },
+  {
+    "revision": "977ecf7be74437c437106e00ad13f379",
+    "url": "/BlueStacksTask/static/media/Bitmap.977ecf7b.png"
+  },
+  {
+    "revision": "88184584c57b10ac821a5f5b66264bdc",
+    "url": "/BlueStacksTask/static/media/bsicon.88184584.svg"
+  }
+]);
